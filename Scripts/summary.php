894c51c760b1e8eb721bc94413d7d6ab468b932d
PeratX <1215714524@qq.com>
2019-08-30 09:58:11 +0800

    Scripts: add summary.php which can generate CSV that contained all Flash PNs

---
+++ Scripts/summary.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * iTXTech FlashDetector
+ *
+ * Copyright (C) 2018-2019 iTX Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//Originally custimized for Fangli Technology Ltd.
+
+require_once "env.php";
+
+use iTXTech\FlashDetector\Constants;
+use iTXTech\FlashDetector\FlashDetector;
+use iTXTech\FlashDetector\Property\Classification;
+use iTXTech\SimpleFramework\Util\StringUtil;
+
+$fdb = FlashDetector::getFdb();
+unset($fdb["info"]);
+
+$csv = "Part Number,Vendor,Density,Cell Level,Package,CE Pins,Flash IDs" . PHP_EOL;
+
+foreach($fdb as $vendor){
+	foreach($vendor as $pn => $info){
+		$i = FlashDetector::detect($pn, true);
+		$ids = $i->getFlashId();
+		if($ids != null and $i->getDensity() > 0){
+			$cell = $i->getCellLevel();
+			$ce = ($i->getClassification() ?? new Classification())->getCe();
+			if(($i->getExtraInfo()[Constants::ENTERPRISE] ?? false) === true){
+				$cell = "e" . $cell;
+			}
+			$csv .= $pn . "," . strtoupper($i->getManufacturer()) . "," .
+				FlashDetector::getHumanReadableDensity($i->getDensity(), true) . "," .
+				$cell . "," . str_replace(",", " ", $i->getPackage()) . "," . $ce . "," .
+				implode(" ", $ids) . PHP_EOL;
+		}
+	}
+}
+
+file_put_contents("summary.csv", $csv);
+
+$csv = "Micron FBGA Code,Part Number,Density,Cell Level,Package,CE Pins,Flash IDs" . PHP_EOL;
+
+$mdb = FlashDetector::getMdb();
+
+foreach($mdb as $vendor){
+	foreach($vendor as $code => $pns){
+		if(!is_array($pns)){
+			$pns = [$pns];
+		}
+		$info = FlashDetector::detect($pns[0]);
+		$ce = ($info->getClassification() ?? new Classification())->getCe();
+		$ids = $info->getFlashId() ?? [];
+		$d = $info->getDensity() ?? 0;
+		$cell = $info->getCellLevel();
+		if(($info->getExtraInfo()[Constants::ENTERPRISE] ?? false) === true){
+			$cell = "e" . $cell;
+		}
+		$csv .= $code . "," . implode("  ", $pns) . "," .
+			FlashDetector::getHumanReadableDensity($d, true) . "," . $cell .
+			"," . str_replace(",", " ", $info->getPackage()) . "," . $ce . "," .
+			implode("  ", $ids) . PHP_EOL;
+	}
+}
+
+file_put_contents("micron_summary.csv", $csv);
+
