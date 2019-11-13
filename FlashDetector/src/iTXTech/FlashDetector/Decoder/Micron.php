0ad0dc043bda96630c0bbdfc99e2d7b1c0171a86
PeratX <1215714524@qq.com>
2019-11-13 19:32:35 +0800

    Micron: 336G

--- FlashDetector/src/iTXTech/FlashDetector/Decoder/Micron.php
+++ FlashDetector/src/iTXTech/FlashDetector/Decoder/Micron.php
@@ -87,6 +87,7 @@ class Micron extends Decoder{
 		"84G" => 84 * Constants::DENSITY_GBITS,
 		"128G" => 128 * Constants::DENSITY_GBITS,
 		"256G" => 256 * Constants::DENSITY_GBITS,
+		"336G" => 336 * Constants::DENSITY_GBITS,
 		"384G" => 384 * Constants::DENSITY_GBITS,
 		"512G" => 512 * Constants::DENSITY_GBITS,
 		"768G" => 768 * Constants::DENSITY_GBITS,
