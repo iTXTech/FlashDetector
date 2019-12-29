47c7c537995fd035fdd692fa206fc946dbb84004
PeratX <1215714524@qq.com>
2019-12-29 14:34:24 +0800

    Scripts: update ChipGeniusFlashListGenerator

--- Scripts/cgflgen.php
+++ Scripts/cgflgen.php
@@ -33,26 +33,44 @@ foreach(FlashDetector::getFdb()->getIddb()->getFlashIds() as $id){
 		$pn = explode(" ", $id->getPartNumbers()[0])[1];
 		$info = FlashDetector::detect($pn, true);
 		if(StringUtil::endsWith($info->getCellLevel() ?? "", "LC")){
+			$d = "";
 			if($id->getPageSize() == Classification::UNKNOWN_PROP){
-				$data .= $id->getFlashId() . "," . $info->getCellLevel() . ",";
+				continue;
 			}elseif($id->getPageSize() < 1){
-				$data .= $id->getFlashId() . "," . $info->getCellLevel() . "-" . $id->getPageSize() * 1024 . ",";
+				$d .= substr($id->getFlashId(), 0, 8) . "," . $info->getCellLevel() . "-" . $id->getPageSize() * 1024 . ",";
 			}else{
-				$data .= $id->getFlashId() . "," . $info->getCellLevel() . "-" . $id->getPageSize() . "K,";
+				$d .= substr($id->getFlashId(), 0, 8) . "," . $info->getCellLevel() . "-" . $id->getPageSize() . "K,";
 			}
+			$hasPn = false;
 			foreach($id->getPartNumbers() as $pn){
 				$pn = explode(" ", $pn)[1];
 				$info = FlashDetector::detect($pn);
 				if($info->getClassification() != null and
-					$info->getClassification()->getCe() != Classification::UNKNOWN_PROP){
-					$data .= "[" . $info->getClassification()->getCe() . "CE]" . $pn . "#";
-				}else{
-					$data .= $pn . "#";
+					$info->getClassification()->getCe() > 0){
+					$d .= "[" . $info->getClassification()->getCe() . "CE]" . $pn . "#";
+					$hasPn = true;
 				}
 			}
-			$data = substr($data, 0, strlen($data) - 1) . "\r\n";
+			if($hasPn){
+				$data .= substr($d, 0, strlen($d) - 1) . "\r\n";
+			}
 		}
 	}
+}
+
+if(file_exists("CGFlashList_ORIG.csv")){
+	$index = [];
+	foreach(explode("\r\n", $data) as $flash){
+		$id = explode(",", $flash)[0];
+		$index[$id] = $flash;
+	}
+	foreach(explode("\r\n", file_get_contents("CGFlashList_ORIG.csv")) as $flash){
+		$id = explode(",", $flash)[0];
+		if(!isset($index[$id])){
+			$index[$id] = $flash;
+		}
+	}
+	$data = implode("\r\n", $index);
 }
 
 file_put_contents("CGFlashList.csv", $data);
