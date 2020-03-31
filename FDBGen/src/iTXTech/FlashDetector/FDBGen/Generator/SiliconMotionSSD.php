5d2d68849b85e2ba75b37c5fcebea8b369d5975a
PeratX <1215714524@qq.com>
2020-03-31 11:15:31 +0800

    FDB: version 57 (see fdfdb commits)

--- FDBGen/src/iTXTech/FlashDetector/FDBGen/Generator/SiliconMotionSSD.php
+++ FDBGen/src/iTXTech/FlashDetector/FDBGen/Generator/SiliconMotionSSD.php
@@ -27,24 +27,58 @@ use iTXTech\FlashDetector\Fdb\Fdb;
 use iTXTech\SimpleFramework\Util\StringUtil;
 
 class SiliconMotionSSD extends Generator{
+	private const CONTROLLERS = [
+		"2258XT" => null,
+		"2259XT" => null,
+		"2258" => null,
+		"58XT" => "2258XT",
+		"2259AB" => "2259",
+		"2258AB" => "2258"
+	];
+
 	public static function getDirName() : string{
 		return "smssd";
 	}
 
 	public static function merge(Fdb $fdb, string $data, string $filename) : void{
 		$controller = "SM" . explode("_", $filename)[0];
+		//新版本SM2258XT的Flash.SET格式变成更加完整的列表，包含SM2258系列，SM2259系列的配置
+		//标识前缀也变为 B
+		$prefix = $controller == "SM2258XT" ? "B" : "A";
+		//部分支持不在新版列表中体现，比如SKHY16nm/14nm，Kioxia BiCS2等
+		if($controller == "SM2258XTLEGACY"){
+			$controller = "SM2258XT";
+		}
 		$fdb->getInfo()->addController($controller);
 		$data = explode("\r\n", $data);
 		foreach($data as $k => $config){
-			if(StringUtil::startsWith($config, "A") and
-				!StringUtil::startsWith($data[$k + 1], "A") and
+			if(StringUtil::startsWith($config, $prefix) and
+				!StringUtil::startsWith($data[$k + 1], $prefix) and
 				!StringUtil::endsWith($config, "[END]")){
 				list($flash, $info) = explode("=", $data[$k + 1], 2);
 				list($vendor, $density, $pn) = explode(",", $flash, 3);
+				//更正错误厂商命名
 				$vendor = str_replace(
 					["hynix", "stm", "power flash"],
 					["skhynix", "st", "powerchip"],
 					strtolower($vendor));
+				if($prefix == "B"){
+					$matches = [];
+					preg_match_all('/(?<=\(SM)[^)]+/', $pn, $matches);
+					$matches = $matches[0];
+					if(count($matches) > 0){
+						$matches = $matches[0];
+						if(in_array($matches, array_keys(self::CONTROLLERS))){
+							$con = "SM" . (self::CONTROLLERS[$matches] ?? $matches);
+						}
+					}
+					if(isset($con)){//在prefix为B的模式下，只有有主控后缀的才被加入FDB
+						$controller = $con;
+						unset($con);
+					}else{
+						continue;
+					}
+				}
 				$pn = trim(preg_replace('/\(.*?\)/', '', $pn));
 				if($vendor == "sandisk"){
 					if(StringUtil::startsWith($pn, "SNDK ") and strlen(substr($pn, 5)) > 5){
@@ -83,10 +117,11 @@ class SiliconMotionSSD extends Generator{
 				for($i = 0; $i < 6; $i++){
 					$id .= $rawId[$i];
 				}
-
-				$fdb->getPartNumber($vendor, $pn, true)
-					->addFlashId($id)
-					->addController($controller);
+				if($pn != "TSB"){
+					$fdb->getPartNumber($vendor, $pn, true)
+						->addFlashId($id)
+						->addController($controller);
+				}
 				$fdb->getIddb()->getFlashId($id, true)->addController($controller);
 			}
 		}
