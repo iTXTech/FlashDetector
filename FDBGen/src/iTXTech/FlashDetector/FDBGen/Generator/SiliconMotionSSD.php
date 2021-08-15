5d47e1832af1d34b0128e135ef8cefe1219cdcb4
PeratX <peratx@itxtech.org>
2021-08-15 15:19:46 +0800

    fdb: version 70

--- FDBGen/src/iTXTech/FlashDetector/FDBGen/Generator/SiliconMotionSSD.php
+++ FDBGen/src/iTXTech/FlashDetector/FDBGen/Generator/SiliconMotionSSD.php
@@ -28,7 +28,7 @@ use iTXTech\FlashDetector\Decoder\SpecTek;
 use iTXTech\FlashDetector\Fdb\Fdb;
 use iTXTech\SimpleFramework\Util\StringUtil;
 
-class SiliconMotionSSD extends Generator{
+class SiliconMotionSSD extends Generator {
 	private const CONTROLLERS = [
 		"2258XT" => null,
 		"2259XT" => null,
@@ -38,25 +38,25 @@ class SiliconMotionSSD extends Generator{
 		"2258AB" => "2258"
 	];
 
-	public static function getDirName() : string{
+	public static function getDirName(): string {
 		return "smssd";
 	}
 
-	public static function merge(Fdb $fdb, string $data, string $filename) : void{
+	public static function merge(Fdb $fdb, string $data, string $filename): void {
 		$controller = "SM" . explode("_", $filename)[0];
 		//新版本SM2258XT的Flash.SET格式变成更加完整的列表，包含SM2258系列，SM2259系列的配置
 		//标识前缀也变为 B
 		$prefix = $controller == "SM2258XT" ? "B" : "A";
 		//部分支持不在新版列表中体现，比如SKHY16nm/14nm，Kioxia BiCS2等
-		if($controller == "SM2258XTLEGACY"){
+		if ($controller == "SM2258XTLEGACY") {
 			$controller = "SM2258XT";
 		}
 		$fdb->getInfo()->addController($controller);
-		$data = explode("\r\n", $data);
-		foreach($data as $k => $config){
-			if(StringUtil::startsWith($config, $prefix) and
+		$data = explode("\r\n", str_replace("\r\n\r\n", "\r\n", $data));
+		foreach ($data as $k => $config) {
+			if (StringUtil::startsWith($config, $prefix) and
 				!StringUtil::startsWith($data[$k + 1], $prefix) and
-				!StringUtil::endsWith($config, "[END]")){
+				!StringUtil::endsWith($config, "[END]")) {
 				list($flash, $info) = explode("=", $data[$k + 1], 2);
 				list($vendor, $density, $pn) = explode(",", $flash, 3);
 				//更正错误厂商命名
@@ -64,46 +64,46 @@ class SiliconMotionSSD extends Generator{
 					["hynix", "stm", "power flash"],
 					["skhynix", "st", "powerchip"],
 					strtolower($vendor));
-				if($prefix == "B"){
+				if ($prefix == "B") {
 					$matches = [];
 					preg_match_all('/(?<=\(SM)[^)]+/', $pn, $matches);
 					$matches = $matches[0];
-					if(count($matches) > 0){
+					if (count($matches) > 0) {
 						$matches = $matches[0];
-						if(in_array($matches, array_keys(self::CONTROLLERS))){
+						if (in_array($matches, array_keys(self::CONTROLLERS))) {
 							$con = "SM" . (self::CONTROLLERS[$matches] ?? $matches);
 						}
 					}
-					if(isset($con)){//在prefix为B的模式下，只有有主控后缀的才被加入FDB
+					if (isset($con)) {//在prefix为B的模式下，只有有主控后缀的才被加入FDB
 						$controller = $con;
 						unset($con);
-					}else{
+					} else {
 						continue;
 					}
 				}
 				$pn = trim(preg_replace('/\(.*?\)/', '', $pn));
-				if($vendor == "sandisk"){
-					if(StringUtil::startsWith($pn, "SNDK ") and strlen(substr($pn, 5)) > 5){
+				if ($vendor == "sandisk") {
+					if (StringUtil::startsWith($pn, "SNDK ") and strlen(substr($pn, 5)) > 5) {
 						$pn = str_replace(["-8G", "-16G", "-32G", "-64G"],
 							["-008G", "-016G", "-032G", "-064G"],
 							str_replace(["  ", " "], "-", substr($pn, 5)));
 					}
 					$pn = explode("_", str_replace(["Toggle)", " "], ["", "_"], $pn));
-					if(StringUtil::startsWith($pn[count($pn) - 1], "DDR")){
+					if (StringUtil::startsWith($pn[count($pn) - 1], "DDR")) {
 						unset($pn[count($pn) - 1]);
 					}
 					$pn = str_replace(["---", "--"], "-", implode("-", $pn));
-					if(StringUtil::endsWith($pn, "-")){
+					if (StringUtil::endsWith($pn, "-")) {
 						$pn = substr($pn, 0, strlen($pn) - 1);
 					}
-				}else{
-					foreach(["-", "_", " "] as $char){
-						if(StringUtil::contains($pn, $char)){
+				} else {
+					foreach (["-", "_", " "] as $char) {
+						if (StringUtil::contains($pn, $char)) {
 							$pn = trim(explode($char, $pn)[0]);
 						}
 					}
 				}
-				switch($vendor){
+				switch ($vendor) {
 					case "spectek":
 						$pn = SpecTek::removePackage($pn);
 						break;
@@ -116,10 +116,10 @@ class SiliconMotionSSD extends Generator{
 				}
 				$rawId = explode(",", $info);
 				$id = "";
-				for($i = 0; $i < 6; $i++){
+				for ($i = 0; $i < 6; $i++) {
 					$id .= $rawId[$i];
 				}
-				if($pn != "TSB"){
+				if ($pn != "TSB") {
 					$fdb->getPartNumber($vendor, $pn, true)
 						->addFlashId($id)
 						->addController($controller);
--- FlashDetector/src/iTXTech/FlashDetector/Fdb/Fdb.php
+++ FlashDetector/src/iTXTech/FlashDetector/Fdb/Fdb.php
@@ -24,7 +24,7 @@ namespace iTXTech\FlashDetector\Fdb;
 
 use iTXTech\FlashDetector\Arrayable;
 
-class Fdb extends Arrayable{
+class Fdb extends Arrayable {
 	/** @var Info */
 	protected $info;
 	/** @var Vendor[] */
@@ -32,12 +32,12 @@ class Fdb extends Arrayable{
 	/** @var Iddb */
 	protected $iddb;
 
-	public function __construct(array $arr = null){
-		if($arr != null){
+	public function __construct(array $arr = null) {
+		if ($arr != null) {
 			$this->info = new Info($arr["info"]);
 			$this->iddb = new Iddb($arr["iddb"]);
 			unset($arr["info"], $arr["iddb"]);
-			foreach($arr as $vendor => $pns){
+			foreach ($arr as $vendor => $pns) {
 				$this->vendors[$vendor] = new Vendor($vendor, $pns);
 			}
 		}
@@ -46,34 +46,34 @@ class Fdb extends Arrayable{
 	/**
 	 * @return Vendor[]
 	 */
-	public function getVendors() : array{
+	public function getVendors(): array {
 		return $this->vendors;
 	}
 
-	public function getVendor(string $vendor) : ?Vendor{
-		return $this->vendors[strtolower($vendor)] ?? null;
+	public function getVendor(string $vendor): ?Vendor {
+		return $this->vendors[trim(strtolower($vendor))] ?? null;
 	}
 
-	public function setVendors(array $vendors){
+	public function setVendors(array $vendors) {
 		$this->vendors = $vendors;
 	}
 
-	public function getPartNumber(string $vendor, string $partNumber, bool $real = false) : ?PartNumber{
-		$vendor = strtolower($vendor);
-		$partNumber = strtoupper($partNumber);
-		if(isset($this->vendors[$vendor])){
+	public function getPartNumber(string $vendor, string $partNumber, bool $real = false): ?PartNumber {
+		$vendor = trim(strtolower($vendor));
+		$partNumber = trim(strtoupper($partNumber));
+		if (isset($this->vendors[$vendor])) {
 			$pn = $this->vendors[$vendor]->getPartNumber($partNumber);
-			if($pn != null){
+			if ($pn != null) {
 				return $real ? $pn : clone $pn;
-			}elseif($real){
+			} elseif ($real) {
 				return $this->addPartNumber($vendor, $partNumber);
 			}
 		}
 		return $real ? $this->addPartNumber($vendor, $partNumber) : null;
 	}
 
-	private function addPartNumber(string $vendor, string $partNumber) : PartNumber{
-		if(!isset($this->vendors[$vendor])){
+	private function addPartNumber(string $vendor, string $partNumber): PartNumber {
+		if (!isset($this->vendors[$vendor])) {
 			$this->vendors[$vendor] = new Vendor($vendor);
 		}
 		$pn = new PartNumber($partNumber);
@@ -81,28 +81,28 @@ class Fdb extends Arrayable{
 		return $pn;
 	}
 
-	public function hasPartNumber(string $vendor, string $partNumber) : bool{
+	public function hasPartNumber(string $vendor, string $partNumber): bool {
 		return $this->getPartNumber($vendor, $partNumber) != null;
 	}
 
-	public function getInfo() : Info{
+	public function getInfo(): Info {
 		return $this->info;
 	}
 
-	public function setIddb(Iddb $iddb){
+	public function setIddb(Iddb $iddb) {
 		$this->iddb = $iddb;
 	}
 
-	public function getIddb() : Iddb{
+	public function getIddb(): Iddb {
 		return $this->iddb;
 	}
 
-	public function toArray() : array{
+	public function toArray(): array {
 		$arr = [
 			"info" => $this->info->toArray(),
 			"iddb" => $this->iddb->toArray()
 		];
-		foreach($this->vendors as $vendor){
+		foreach ($this->vendors as $vendor) {
 			$arr[$vendor->getName()] = $vendor->toArray();
 		}
 		return $arr;
